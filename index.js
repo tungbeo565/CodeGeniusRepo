7e7170e4388791f52deae5cf84803e164b2d3bee
tungbeo565 <jimmyphilepun0suy@hotmail.com>
2024-05-10 05:10:22 +0700

    start project commit

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-function zigzagLevelOrder(root) {
-  if (!root) return [];
-  const result = [];
-  const queue = [root];
-  let zigzag = false;
-  while (queue.length) {
-    const levelSize = queue.length;
-    const currentLevel = [];
-    for (let i = 0; i < levelSize; i++) {
-      const node = queue.shift();
-      if (zigzag) currentLevel.unshift(node.val);
-      else currentLevel.push(node.val);
-      if (node.left) queue.push(node.left);
-      if (node.right) queue.push(node.right);
-    }
-    result.push(currentLevel);
-    zigzag = !zigzag;
-  }
-  return result;
-}
+const message = "Hello world!";
+const messageLength = message.length;
+console.log(messageLength); // 13
